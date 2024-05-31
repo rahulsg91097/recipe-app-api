3d906b3617bbb370d81be2301fa01ed9b6974dde
Grahulprdxn <rahulg@prdxn.com>
2024-05-31 14:38:33 +0530

    Solved flake8 errors

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,8 +2,10 @@
 
 
 def add(x, y):
+
     # Add x and y and return result
     return x + y
+
 
 def subtract(x, y):
     """Subtract x and y and return result"""
--- app/app/tests.py
+++ app/app/tests.py
@@ -2,6 +2,7 @@ from django.test import SimpleTestCase
 
 from . import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
 
